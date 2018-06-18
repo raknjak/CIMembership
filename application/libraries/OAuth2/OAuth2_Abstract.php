bba1d3fec64ee18b27d9a2f6ecdc64589d71edfd
raknjak <bruno.matthys@gmail.com>
2018-06-18 22:33:55 +0200

    BUGFIX - also belongs to the disable all setting not working ( + typo in OAuth2 lib file)

--- application/libraries/OAuth2/OAuth2_Abstract.php
+++ application/libraries/OAuth2/OAuth2_Abstract.php
@@ -1,4 +1,4 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 abstract class OAuth2_Abstract
 {
